754015b7c75ae88ebd2578ec46db1bdc4c80c10f
padster <padsterpat@gmail.com>
2017-04-26 15:50:02 -0700

    Added autocorrelation graphs for NN layers

--- analysis.py
+++ analysis.py
@@ -3,8 +3,11 @@ Code for running some custom analysis on the sound files.
 Not for style transfer itself, but for testing out features to use for the transfer
 """
 
+import lasagne
 import matplotlib.pyplot as plt
 import numpy as np
+import theano
+import theano.tensor as T
 import skimage.transform
 import scipy
 
@@ -12,6 +15,7 @@ import audioUtils
 import imageTransfer
 import imgUtils
 import mfcc
+import vggnet
 import viz
 
 def crossCorrelate(a, b):
@@ -84,9 +88,64 @@ def runMFCCAutocorrelations():
     ax[1].legend()
     plt.show()
 
+def runVggAutocorrelations(layers, showRows=True):
+    print "Loading files..."
+    r1, s1 = audioUtils.loadFirst10s('data/rock17.wav')
+    r2, s2 = audioUtils.loadFirst10s('data/reggae07.wav')
+    s1 = audioUtils.preprocess(s1, r1)
+    s2 = audioUtils.preprocess(s2, r2)
+    assert r1 == r2 and len(s1) == len(s2) # should be 661794 = 2 * 3 * 7^2 * 2251 for marsyas
 
+    print "Generating spectrograms..."
+    spec1 = audioUtils.toSpectrogram(s1)
+    spec2 = audioUtils.toSpectrogram(s2)
 
+    print "Generating mel spectrograms..."
+    melSpec1 = audioUtils.toMelSpectrogram(spec1)
+    melSpec2 = audioUtils.toMelSpectrogram(spec2)
 
+    print "Preprocessing mel spectrogram images"
+    mx1, mn1 = np.max(melSpec1), np.min(melSpec1)
+    mx2, mn2 = np.max(melSpec2), np.min(melSpec2)
+    fft1Img = ((melSpec1 - mn1) / (mx1 - mn1) * 256).astype('uint8')
+    fft2Img = ((melSpec2 - mn2) / (mx2 - mn2) * 256).astype('uint8')
+    # Each will be 96 x 854, no need to downsample
+    _, fft1ImgProc = imgUtils.preprocess(fft1Img)
+    _, fft2ImgProc = imgUtils.preprocess(fft2Img)
+
+    print "Building VGG.."
+    _, _, h, w = fft1ImgProc.shape
+    net = vggnet.buildVgg(w, h)
+
+    for layer in layers:
+        print 'Compute activations for layer %s' % layer
+        input_im_theano = T.tensor4()
+        output = lasagne.layers.get_output(net[layer], input_im_theano)
+        features1 = theano.shared(output.eval({input_im_theano: fft1ImgProc})).eval()
+        features2 = theano.shared(output.eval({input_im_theano: fft2ImgProc})).eval()
+        _, depth, rows, cols = features1.shape
+
+        ax = viz.cleanSubplots(2, 1, axes=True)
+        if showRows:
+            ax[0].set_title('Content Row Autocorrelations for layer ' + layer)
+            ax[1].set_title('Style Row Autocorrelations for layer ' + layer)
+            for d in range(96 / rows):
+                for row in range(rows):
+                    row1, row2 = features1[0, d, row, :], features2[0, d, row, :]
+                    ccd1, ccd2 = crossCorrelateDiff(row1), crossCorrelateDiff(row2)
+                    ax[0].plot(ccd1)
+                    ax[1].plot(ccd2)
+            plt.show()
+        else:
+            ax[0].set_title('Content Column Autocorrelations for layer ' + layer)
+            ax[1].set_title('Style Column Autocorrelations for layer ' + layer)
+            for col in range(0, cols, (cols + 63) // 64):
+                col1, col2 = features1[0, 0, :, col], features2[0, 0, :, col]
+                ccd1, ccd2 = crossCorrelateDiff(col1), crossCorrelateDiff(col2)
+                ax[0].plot(ccd1)
+                ax[1].plot(ccd2)
+            plt.show()
 
 if __name__ == '__main__':
-    runMFCCAutocorrelations()
+    #runMFCCAutocorrelations()
+    runVggAutocorrelations(layers=['conv1_1', 'conv2_1', 'conv3_1', 'conv4_1', 'conv5_1'], showRows=False)
